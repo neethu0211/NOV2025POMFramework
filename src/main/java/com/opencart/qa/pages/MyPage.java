08da7a6fe9ff8abce079ba093fd36ae0ad86982a
neethu0211 <neethuchandran0211@gmail.com>
2026-04-07 12:57:24 +0530

    updated MyPage

--- src/main/java/com/opencart/qa/pages/MyPage.java
+++ src/main/java/com/opencart/qa/pages/MyPage.java
@@ -9,4 +9,11 @@ public class MyPage {
 	{
 		System.out.println("hello page");
 	}
+	public void login()
+	{
+		System.out.println("hello login");
+		System.out.println("hello login");
+		System.out.println("hello login");
+		System.out.println("hello login");
+	}
 }
